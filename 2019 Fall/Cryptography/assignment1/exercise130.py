3a0341235b91c935068adc823cc76ef4875d407b
Björn Bebensee <bebensee@snu.ac.kr>
2019-10-01 05:37:34 +0900

    Cleaned up solution

--- 2019 Fall/Cryptography/assignment1/exercise130.py
+++ 2019 Fall/Cryptography/assignment1/exercise130.py
@@ -1,14 +1,6 @@
-# The ciphertext
-c = "WRTCNRLDSAFARWKXFTXCZRNHNYPDTZUUKMPLUSOXNEUDOKLXRMCBKGRCCURR"
-
 # Generate dictionary for alphabet
 alph = {chr(i+97):i for i in range(0,26)}
 invalph = {v: k for k, v in alph.items()}
-
-# Use the dictionary to convert the string to a list of numbers we can permute
-cnum = []
-for x in list(c.lower()):
-    cnum.append(alph[x])
 
 # Permutation dictionary and its inverse
 pi = {0: 23, 1: 13, 2: 24, 3: 0, 4: 7, 5: 15, 6: 14, 7: 6, 8: 25, 9: 16, 10: 22, 11: 1, 12: 19, 13: 18, 14: 5, 15: 11, 16: 17, 17: 2, 18: 21, 19: 12, 20: 20, 21: 4, 22: 10, 23: 9, 24: 3, 25: 8}
@@ -18,29 +10,23 @@ def enc(list, key):
     y = []
     for i, x in enumerate(list):
         z = (key + i - 1) % 26
-        y.append(perm(x) + z % 26)
+        y.append(pi[x] + z % 26)
     return y
 
 def dec(list, key):
     x = []
     for i, y in enumerate(list):
         z = (key + i - 1) % 26
-        x.append(invperm((y - z) % 26))
+        x.append(invpi[(y - z) % 26])
     return x
 
-def perm(x):
-    return pi[x]
-
-def invperm(x):
-    return invpi[x]
-
-def stringtonum(c):
+def string2num(c):
     num = []
     for x in list(c.lower()):
         num.append(alph[x])
     return num
 
-def numtostring(y):
+def num2string(y):
     # Use the dictionary to get a string back
     p = []
     for i in y:
@@ -48,19 +34,14 @@ def numtostring(y):
     p = ''.join(p).upper()
     return p
 
-a = "teststring"
-print(a)
-anum = stringtonum(a)
-print(anum)
-y = enc(anum,17)
-print(y)
-x = dec(y,17)
-print(x)
-p =numtostring(x)
-print(p)
+# The ciphertext
+c = "WRTCNRLDSAFARWKXFTXCZRNHNYPDTZUUKMPLUSOXNEUDOKLXRMCBKGRCCURR"
+
+# Numeric representation of string
+num = string2num(c)
 
 # Exhaustive key search over key space
 for i in range(0,26):
-    y = dec(cnum,i)
-    p = numtostring(y)
-    print(str(i) + ". " + p)
+    y = dec(num,i)
+    p = num2string(y)
+    print("K = " + str(i) + ": " + p)
